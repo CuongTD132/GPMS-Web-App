7984a2afa2c515496c639869137ddc87de1f9653
CuongTD132 <cuongtu532@gmail.com>
2024-08-02 09:46:38 +0700

    feat: get material page

---
+++ src/app/types/material.type.ts
@@ -0,0 +1,10 @@
+export interface Material {
+    id: string;
+    name: string;
+    code: string;
+    consumptionUnit: string;
+    sizeWidthUnit: string;
+    colorCode: string;
+    colorName: string;
+    description: string;
+}
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiURL: 'http://112.213.91.196:2908',
+};
